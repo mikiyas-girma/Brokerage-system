50a7e776f3285d4da4238f20e5907e0f2ca1c13f
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2024-01-24 21:38:44 +0300

    Remove unnecessary code and file and update Bootstrap version

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -6,14 +6,11 @@
 <?php
 
 
-    if(!isset($_SESSION['user_role']))
-    {
-            header("Location: ../index.php");   
-    }
-    else if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == "Subscriber")
-    {
-        header("Location: ../index.php");
-    }
+if (!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+} else if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == "Subscriber") {
+    header("Location: ../index.php");
+}
 
 ?>
 
@@ -30,32 +27,23 @@
 
     <title>Brokerage System Admin</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap  CSS 3.3.2 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
     <link href="css/sb-admin.css" rel="stylesheet">
-    <link href="css/det_custom.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <link rel="stylesheet" href="css/styles.css">
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
+    <!-- loader icon -->
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
 
     <script src="js/jquery.js"></script>
 
-    
+
 
 </head>
 
