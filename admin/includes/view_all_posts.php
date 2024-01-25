2b537eef4a3076131b800c236ead2b69705886d5
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2024-01-26 01:03:38 +0300

    if multiple images are there in the post use the first one to display in the table that show all properties

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -117,6 +117,7 @@ if (isset($_POST['checkBoxArray'])) {
                     $post_user = $row['post_user'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
+                    $post_image = explode(',', $post_image)[0];
                     $post_content = $row['post_content'];
                     $post_tags = $row['post_tags'];
                     $post_comment_count = $row['post_comment_count'];
@@ -139,6 +140,8 @@ if (isset($_POST['checkBoxArray'])) {
                     echo "<td>$post_title</td>";
 
                     echo "<td>$post_status";
+
+
                     echo "<td><img class='img-responsive' width='100' src='../images/$post_image'</td>";
                     echo "<td>$post_tags</td>";
 
